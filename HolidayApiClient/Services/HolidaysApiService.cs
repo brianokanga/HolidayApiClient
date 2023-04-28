6777f2676126e0ca721552fc9c475ae3b47d48b0
Brianokanga <briannokanga@gmail.com>
2023-04-28 12:13:32 +0300

    Added IholidayApiService

---
+++ HolidayApiClient/Services/HolidaysApiService.cs
@@ -0,0 +1,25 @@
+﻿using HolidayApiClient.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HolidayApiClient.Services
+{
+	public class HolidaysApiService : IHolidaysApiService
+	{
+		private static readonly HttpClient client;
+
+		static HolidaysApiService() 
+		{ 
+			client = new HttpClient() 
+			{
+				BaseAddress = new System.Uri("https://date.nager.at")
+			};
+		}
+
+		public Task<List<HolidayModel>> GetHolidays(string countryCode, int year)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
