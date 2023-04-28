8c64ed1a4e46d3b735ea6b13663efb492b28c62c
Brianokanga <briannokanga@gmail.com>
2023-04-28 12:03:12 +0300

    Added holiday model

---
+++ HolidayApiClient/Models/HolidayModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolidayApiClient.Models
+{
+	public class HolidayModel
+	{
+		public string Name { get; set; }
+		public string LocalName { get; set; }
+		public DateTime? Date { get; set; }
+		public string CountryCode { get; set;}
+		public bool	Global { get; set;}
+
+	}
+}
