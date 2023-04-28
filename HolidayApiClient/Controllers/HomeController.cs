cc9e3d06ff57bf9e6b6f246273d86661755cbc5b
Brianokanga <briannokanga@gmail.com>
2023-04-28 12:44:46 +0300

    Added controller method to get holidays

--- HolidayApiClient/Controllers/HomeController.cs
+++ HolidayApiClient/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using HolidayApiClient.Models;
+using HolidayApiClient.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,26 +14,38 @@ namespace HolidayApiClient.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IHolidaysApiService _holidaysApiService;
+
+
 
 		public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
 		}
 
-		public IActionResult Index()
+		public HomeController(IHolidaysApiService holidaysApiService)
 		{
-			return View();
+			_holidaysApiService = holidaysApiService;
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
 
+		
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public async Task<IActionResult> Index(string countryCode, int year)
+		{
+			List<HolidayModel> holidays = new List<HolidayModel>();
+			holidays = await _holidaysApiService.GetHolidays(countryCode, year);
+
+			return View(holidays);
 		}
 	}
 }
