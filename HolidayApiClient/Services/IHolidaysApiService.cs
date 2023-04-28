0e09daed9d483920a27b30a0abb8191459bab4b2
Brianokanga <briannokanga@gmail.com>
2023-04-28 12:05:41 +0300

    Added IHolidayInterface

--- HolidayApiClient/Services/IHolidaysApiService.cs
+++ HolidayApiClient/Services/IHolidaysApiService.cs
@@ -1,6 +1,11 @@
-﻿namespace HolidayApiClient.Services
+﻿using HolidayApiClient.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HolidayApiClient.Services
 {
 	public interface IHolidaysApiService
 	{
+		Task<List<HolidayModel>> GetHolidays(string countryCode, int year);
 	}
 }
